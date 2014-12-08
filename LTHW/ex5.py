ec4989c039f70dcf758d9f4dde67ff08374c41d4
Héctor Iván Patricio Moreno <hectorivanpatriciomoreno@gmail.com>
2014-12-07 23:59:29 -0600

    Ex 5

---
+++ LTHW/ex5.py
@@ -0,0 +1,19 @@
+my_name = 'Héctot Iván Patricio Moreno'
+my_age = 25 # not a lie
+my_height = 68 # inches
+my_weight = 180 # lbs
+my_eyes = 'Brown'
+my_teeth = 'White'
+my_hair = 'Black'
+
+
+print "Let's talk about %s" % my_name
+print "He's %d inches tall." % my_height
+print "He's %d pounds heavy." % my_weight
+print "Actually that's not too heavy."
+print "He's got %s eyes and %s hair." % (my_eyes, my_hair)
+print "His teeth are usally %s depending on the coffee." % my_teeth
+
+
+print "If I add %d, %d, and %d I get %d". % (
+    my_age, my_height, my_weight, my_age )
