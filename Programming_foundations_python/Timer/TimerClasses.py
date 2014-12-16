cb028b6d2097b48a46b086042c6f3361c61be0c6
Héctor Iván Patricio Moreno <hectorivanpatriciomoreno@gmail.com>
2014-12-16 10:07:49 -0600

    Timer with 3 times

--- Programming_foundations_python/Timer/TimerClasses.py
+++ Programming_foundations_python/Timer/TimerClasses.py
@@ -1,4 +1,26 @@
 # -*- coding=utf-8 -*-
+import webbrowser
+import time
+
 
 class Timer():
-    pass
+    def __init__(self, video='', time_sleep=60*60*2):
+        self.video = video
+        self.time  = time_sleep
+
+    def launchVideo(self,time_sleep=None):
+        if isinstance(time_sleep, int):
+            self.time = time_sleep
+        time.sleep(self.time)
+        webbrowser.open(self.video)
+
+    def startTimer(self):
+        for x in range(1,4):
+            print "Time %d" % x
+            self.launchVideo(3)
+    def main(self):
+        self.startTimer()
+
+if __name__ == '__main__':
+    t = Timer("http://vimeo.com/93003441")
+    t.main()
--- Programming_foundations_python/Timer/tests/TimerTest.py
+++ Programming_foundations_python/Timer/tests/TimerTest.py
@@ -6,10 +6,15 @@ import TimerClasses
 
 class TimerTest(unittest.TestCase):
     def setUp(self):
-        self.timer = TimerClasses.Timer()
+        self.timer = TimerClasses.Timer(video='http://vimeo.com/93003441')
 
     def testClassExists(self):
         self.assertEquals('Timer', self.timer.__class__.__name__)
 
+    def testHasCorrectURL(self):
+        self.assertEquals('http://vimeo.com/93003441', self.timer.video)
+
+    def testTimeIs2Hours(self):
+        self.assertEquals(7200,self.timer.time)
 if __name__ == '__main__':
     unittest.main()
