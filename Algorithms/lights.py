5c3658edef1f1e6c552cbb6623297a9ad68760bd
Héctor Iván Patricio Moreno <hectorivanpatriciomoreno@gmail.com>
2016-03-09 13:53:37 -0600

    Testing lights

--- Algorithms/lights.py
+++ Algorithms/lights.py
@@ -1,22 +1,21 @@
-def lightBulbs(lights, n):
-    for i in range(n):
-        j = -1
-        l = lights[:]
-        for bulb in l:
-            if l[j]:
-                lights[j+1] = int(not bulb)
-            j += 1
-        print(lights)
-    return lights
+# def lightBulbs(lights, n):
+#     for :
+#         j = -1
+#         l = lights[:]
+#         for bulb in l:
+#             if l[j]:
+#                 lights[j+1] = int(not bulb)
+#             j += 1
+#         print(lights)
+#     return lights
+
 
 def rlightBulbs(l, n):
-    print(n)
-    r = l[:]
-    for j in range(len(l)):
-        if l[j-1]:
-            r[j] = int(not l[j])
-
-    print(r)
-    return r if n == 1 else rlightBulbs(r, n-1)
+    while n:
+        for i, b in enumerate(l[:]):
+            l[i + 1] = b ^ l[i+1]
+        n -= 1
+        print(l)
+    return l
 
 print(rlightBulbs([0, 1, 1, 0, 1], 2))
