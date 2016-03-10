53b9bf3dc288757c57957bd4ee56d2503a3c131d
Héctor Iván Patricio Moreno <hectorivanpatriciomoreno@gmail.com>
2016-03-10 16:35:39 -0600

    Using list comprehension

--- Algorithms/MatrixMult.py
+++ Algorithms/MatrixMult.py
@@ -1,19 +1,13 @@
 def MatrixMultiplication(a, b):
     r = [0,1,2]
-    z = []
-    for i in r:
-        t = [0] * 3
-        for j in r:
-            for k in r:
-                t[j] += a[i][k] * b[k][j]
-        z += [t]
-    return z
+    return [[sum([a[i][k] * b[k][j] for k in r])  for j in r] for i in r]
 
 a = [[1,2,3],
-           [4,5,6],
-           [7,8,1]]
+     [4,5,6],
+     [7,8,1]]
+
 b = [[1,2,3],
-           [3,2,1],
-           [4,5,2]]
+     [3,2,1],
+     [4,5,2]]
 
 print(MatrixMultiplication(a,b))
