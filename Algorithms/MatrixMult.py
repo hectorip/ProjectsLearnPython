657fbaf064386a73081b514cbabd5c1b723914a8
Héctor Iván Patricio Moreno <hectorivanpatriciomoreno@gmail.com>
2016-03-10 16:19:46 -0600

    Tests

--- Algorithms/MatrixMult.py
+++ Algorithms/MatrixMult.py
@@ -1,4 +1,4 @@
-def MatrixMultiplication(a,b):
+def MatrixMultiplication(a, b):
     r = [0,1,2]
     z = []
     for i in r:
@@ -9,3 +9,11 @@ def MatrixMultiplication(a,b):
         z += [t]
     return z
 
+a = [[1,2,3],
+           [4,5,6],
+           [7,8,1]]
+b = [[1,2,3],
+           [3,2,1],
+           [4,5,2]]
+
+print(MatrixMultiplication(a,b))
