b79a658fe19ec0c7a88115402b40dfd6a941b898
Héctor Iván Patricio Moreno <hectorivanpatriciomoreno@gmail.com>
2016-03-11 17:11:06 -0600

    Beautiful solution

--- Algorithms/primes.py
+++ Algorithms/primes.py
@@ -1,19 +1,14 @@
 def simplifiedArray(a):
-    d = a[0]
-    s = [d]
-    p = u(d)
-    i= 0
-    for x in a[1:]:
-        q = u(x)
-        if q ^ p:
+    s = [a[0]]
+    for x, y in zip(a[1:], a[:-1]):
+        i = len(s) - 1
+        if u(x) ^ u(y):
             s += [x]
-            i +=1
-            p = q
         else:
             s[i] += x
-            
+
     return s == a and a or simplifiedArray(s)
 
-u = lambda n: n>1 and all([(n%j) for j in range(2, int(n**0.5)+1)])
+u = lambda n: n>1 and all([(n%j) for j in range(2, int(n**0.5)+1)]) 
 
 print(simplifiedArray([-3, 4, 5, 2, 0, -10]))
