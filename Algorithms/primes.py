cf2f8593d71a7687dbb8fc71ebc52f08002afba3
Héctor Iván Patricio Moreno <hectorivanpatriciomoreno@gmail.com>
2016-03-11 00:54:48 -0600

    slow solutions

--- Algorithms/primes.py
+++ Algorithms/primes.py
@@ -1,34 +1,27 @@
-def simplifiedArray(a):
-    r = o()
+
+def simplifiedArray(a, R=[2,3]):
+    for i in range(max(R)+2,max(a) + 1,2):
+        q = 1
+        for x in filter(lambda r: r<i/2, R):
+            if not(i % x):
+                q = 0
+                break
+        if q:
+            R += [i]
+    print(R)
     c = [a[0]]
     s = []
-    p = f(a[0])
+    p = a[0] in R
     for x in a[1:]:
-        if f(x) == p:
+        if (x in R) == p:
             c += [x]
         else:
             s += [sum(c)]
             c = [x]
             p = not p
     s += [sum(c)]
-    return a if s == a else simplifiedArray(s)
+    return a if s == a else simplifiedArray(s,R)
 
 
-def o():
-    p = [2,3,5,7,9]
-    for i in range(11,999,2):
-        q = 1
-        for x in p:
-            if not(i % x):
-                q = 0
-                break
-        if q:
-            p += [i]
-    return p
-def f(n, p):
-    if n in p:
-        return True, p
-    else:
-        return False, p
 
-print( simplifiedArray([1, 2, 3, 5, 6, 4, 2, 3]) )
+print( simplifiedArray([1, 2, 1009, 5, 6, 4, 2, 30000]) )
